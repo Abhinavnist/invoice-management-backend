8e6687a8f95d38f9ec7f421f3dec84a15cc7d5a9
abhinavnist <abhinavpatna55@gmail.com>
2024-10-20 20:49:24 +0530

    added db

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    console.log("connection")
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/invoice")
+    const conn = await mongoose.connect(
+      "mongodb+srv://abhinavkum07:abhinavkum07@lms.uh6brsm.mongodb.net/?retryWrites=true&w=majority&appName=Lms"
+    )
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
